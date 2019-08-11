7c6177d5c17f3c924f8eccc0c41c543d5cc36c71
greigs <greig2@gmail.com>
2019-08-11 15:51:36 +0100

    wip

--- js/game.js
+++ js/game.js
@@ -10,14 +10,16 @@ var container, stats;
       var gameArray = [];
       var group = new THREE.Group();
 
-      var cubeLength = 15,
+      var cubeLength = 32,
           cubeDistance = 11,
-          cubeScale = 8;
+          cubeScale = 8,
+          cubeDepth = 32,
+          cubeHeight = 32;
 
       var tick = 0,
-          refreshRate = 30;
+          refreshRate = 5;
 
-      var startingComplexity = 0.75;
+      var startingComplexity = 0.05;
 
       init();
       animate();
@@ -48,39 +50,38 @@ var container, stats;
 
         // Create the game array
         var geom = new THREE.BoxGeometry( 1, 1, 1 );
-        var color = new THREE.Color();
 
-        for ( var i = 0; i < cubeLength; i ++ ) {
+        for ( var i = 0; i < cubeDepth; i ++ ) {
 
           gameArray[i] = [];
 
-          for ( var j = 0; j < cubeLength; j ++ ) {
+          for ( var j = 0; j < cubeHeight; j ++ ) {
 
             gameArray[i][j] = [];
 
             for ( var k = 0; k < cubeLength; k ++ ) {
+              if (k === 0 || k === cubeLength - 1 || j === 0 || j === cubeHeight - 1 || i === 0 || i === cubeDepth - 1){
+                var tempmat = new THREE.MeshBasicMaterial({ color: 0x000000 } );
 
-              var tempmat = new THREE.MeshBasicMaterial({ color: 0xffffff } );
+                if(Math.random() < startingComplexity){
+                  tempmat = new THREE.MeshBasicMaterial({ color: 0x333333 } );
+                  gameArray[i][j][k] = 1;
+                }
+                else {
+                  gameArray[i][j][k] = 0;
+                }
+                
+                cube = new THREE.Mesh( geom, tempmat );
+                cube.position.x = i * cubeDistance;
+                cube.position.y = j * cubeDistance;
+                cube.position.z = k * cubeDistance;
+                cube.scale.x = cube.scale.y = cube.scale.z = cubeScale;
+                cube.pos = [i,j,k];
 
-              if(Math.random() > startingComplexity){
-                tempmat = new THREE.MeshBasicMaterial({ color: 0x333333 } );
-                gameArray[i][j][k] = true;
+                group.add( cube );
               }
-              else {
-                gameArray[i][j][k] = false;
-              }
-
-              cube = new THREE.Mesh( geom, tempmat );
-              cube.position.x = i * cubeDistance;
-              cube.position.y = j * cubeDistance;
-              cube.position.z = k * cubeDistance;
-              cube.scale.x = cube.scale.y = cube.scale.z = cubeScale;
-              cube.pos = [i,j,k];
-
-              group.add( cube );
-
             }
-          }
+          }          
         }
 
         scene.add(group);
@@ -103,57 +104,69 @@ var container, stats;
 
       function gameTick(){
         // start 3d loop
-        for ( var i = 0; i < cubeLength; i++ ) {
-          for ( var j = 0; j < cubeLength; j++ ) {
+        for ( var i = 0; i < cubeDepth; i++ ) {
+          for ( var j = 0; j < cubeHeight; j++ ) {
             for ( var k = 0; k < cubeLength; k++ ) {
-
-                // start 3d loop test for nearest neighbors
-                var liveNeighbours = 0;
-                for ( var ii = -1; ii <= 1; ii++ ) {
-                  for ( var jj = -1; jj <= 1; jj++ ) {
-                    for ( var kk = -1; kk <= 1; kk++ ) {
-                      var newi = i + ii,
-                          newj = j + jj,
-                          newk = k + kk;
-                      if(newi !== -1 && newj !== -1 && newk !== -1 && newi !== cubeLength && newj !== cubeLength && newk !== cubeLength){
-                        if(gameArray[newi][newj][newk] /*&& ii !== 0 && jj !== 0 && kk !== 0*/){
-                          liveNeighbours++;
+                if (k === 0 || k === cubeLength - 1 || j == 0 || j === cubeHeight - 1  || i === 0 || i === cubeDepth - 1){
+                  // start 3d loop test for nearest neighbors
+                  var liveNeighbours = 0;
+                  for ( var ii = -1; ii <= 1; ii++ ) {
+                    for ( var jj = -1; jj <= 1; jj++ ) {
+                      for ( var kk = -1; kk <= 1; kk++ ) {
+                        var newi = i + ii,
+                            newj = j + jj,
+                            newk = k + kk;
+                        if(newi !== -1 && newj !== -1 && newk !== -1 && newi !== cubeDepth && newj !== cubeHeight && newk !== cubeLength){
+                          if(gameArray[newi][newj][newk] /*&& ii !== 0 && jj !== 0 && kk !== 0*/){
+                            liveNeighbours++;
+                          }
                         }
                       }
                     }
                   }
-                }
+                
                 // end 3d loop test for nearest neighbors -
                 
                 var truth = gameArray[i][j][k];
                 var newtruth = truth;
                 if(newtruth){
-                  
                   // rule 1 & 3
-                  if(liveNeighbours < 1 || liveNeighbours > 3){
-                    newtruth = false;
+                  if(liveNeighbours < 2 || liveNeighbours > 3){
+                    newtruth = 0;
                   }
+                  else{
+                    newtruth++;
+                    //  alteration
+                    if (newtruth > 4){
+                      newtruth = 0
+                    }
+                  }                
                 }
                 else {
                   // rule 4
                   if(liveNeighbours === 3){
-                    newtruth = true;
+                    newtruth++;
                   }
                 }
                 // if change -
                 if(truth !== newtruth){
-                  var groupPosition = (i * cubeLength * cubeLength) + (j * cubeLength) + k;
-                  // set new colour
-                  if(newtruth){
-                    scene.children[1].children[groupPosition].material.color.setHex(0x333333);
+                  var groupPosition = (i * cubeLength * cubeLength) + (j * cubeHeight) + k;
+
+                  if (scene.children[1].children[groupPosition] ){
+                    // set new colour
+                    if(newtruth){
+                      var color = shadeColor("#3333FF", newtruth * 30);
+                      scene.children[1].children[groupPosition].material.color.setHex(color);
+                    }
+                    else{
+                      scene.children[1].children[groupPosition].material.color.setHex(0x000000);
+                    }
+                    gameArray[i][j][k] = newtruth;
                   }
-                  else{
-                    scene.children[1].children[groupPosition].material.color.setHex(0xffffff);
-                  }
-                  gameArray[i][j][k] = newtruth;
                 }
             }
           }
+        }
         }
         // end 3d loop
       }
@@ -178,3 +191,24 @@ var container, stats;
         controls.update();
         renderer.render( scene, camera );
       }
+      
+      function shadeColor(color, percent) {
+
+        var R = parseInt(color.substring(1,3),16);
+        var G = parseInt(color.substring(3,5),16);
+        var B = parseInt(color.substring(5,7),16);
+    
+        R = parseInt(R * (100 + percent) / 100);
+        G = parseInt(G * (100 + percent) / 100);
+        B = parseInt(B * (100 + percent) / 100);
+    
+        R = (R<255)?R:255;  
+        G = (G<255)?G:255;  
+        B = (B<255)?B:255;  
+    
+        var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+        var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+        var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+    
+        return parseInt("0x"+RR+GG+BB, 16);
+    }
